4c3187c1fde2f235ca5ecd71ee63c9070db9a2c8
Maria Luz Goyeneche <marialuz.goyeneche@gmail.com>
2024-11-08 14:50:21 -0300

    Agrego cambios que mejoran la funcionalidad

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,21 @@
-let niños = 0, adolescentes = 0, adultos = 0;
+let niños = 0;
+let adolescentes = 0;
+let  adultos = 0;
+
 while (true) {
-  let edad = prompt("Ingresa una edad (o escribe 'salir' para terminar):");
+  let edad = parseInt(prompt("Ingresa una edad (o escribe 'salir' para terminar):"));
   if (edad === 'salir') break;
-  edad = parseInt(edad);
-  debugger;
+
   if (edad !== edad || edad <= 0) {
     alert("Por favor, ingresa una edad válida.");
     continue;
   }
-  if (edad <= 12) niños++;
-  else if (edad <= 19) adolescentes++;
-  else adultos++;
+  if (edad <= 12) {
+    niños++;
+  }else if (edad <= 19) {
+    adolescentes++;
+  }else {
+    adultos++;
+}
 }
 console.log(`Niños: ${niños}, Adolescentes: ${adolescentes}, Adultos: ${adultos}`);
